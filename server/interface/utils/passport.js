b800fef0484bbfb15148ce868ca3ee44e1c9608e
XJkey <>
2020-01-02 23:43:40 +0800

    no message

--- server/interface/utils/passport.js
+++ server/interface/utils/passport.js
@@ -2,11 +2,11 @@ import passport from 'koa-passport';
 import LocalStrategy from 'passport-local';
 import UserModel from '../../dbs/models/users';
 
-passport.use(new LocalStrategy(function (username, password, done) {
+passport.use(new LocalStrategy(async function (username, password, done) {
   let where = {
     username
   };
-  let result = UserModel.findOne(where);
+  let result =await UserModel.findOne(where);
   if (result != null) {
     if (result.password === password) {
       return done(null, result)
